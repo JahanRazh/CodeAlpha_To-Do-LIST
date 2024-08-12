ea5ae0f8dc1ca1a32128a216920ac4a882eee7f6
jahanRazh <jahanrazh@gmail.com>
2024-08-13 01:46:15 +0530

    add

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('addTaskButton').addEventListener('click', addTask);
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const taskList = document.getElementById('taskList');
+
+        const li = document.createElement('li');
+        li.textContent = taskText;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => {
+            taskList.removeChild(li);
+        });
+
+        li.appendChild(deleteButton);
+        taskList.appendChild(li);
+
+        taskInput.value = '';
+    }
+}
